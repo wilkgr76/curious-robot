5b4f6c7a1444627d0001d6a5ff6d3c55baba80eb
wilkgr <wilkgr76@users.noreply.github.com>
2016-08-06 18:30:33 +1000

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Curious Robot
 A WIP sidescroller. Not much but a plan so far, all help appreciated :)
+
+Here are some of my ideas so far:
+- Career mode with storyline
+- Race mode against AI
+
--- start.py
+++ start.py
@@ -1,1 +1,16 @@
-{\rtf1}
+import pygame
+
+background_colour = (255,255,255)
+(width, height) = (300, 200)
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Tutorial 1')
+screen.fill(background_colour)
+# pygame.draw.circle(screen, (0,0,255), (150, 50), 15, 1)
+pygame.display.flip()
+
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
