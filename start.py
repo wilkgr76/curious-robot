29861bfb36d426ba3c2790738d55e2ef99fdf6c3
wilkgr <wilkgr76@users.noreply.github.com>
2016-08-08 09:02:37 +1000

    Add dead grass

--- start.py
+++ start.py
@@ -12,6 +12,7 @@ screen.fill(background_colour)
 # Load Textures
 ground = pygame.image.load('stuff/textures/ground.png') 
 grass = pygame.image.load('stuff/textures/grass.png')
+y_grass = pygame.image.load('stuff/textures/y_grass.png')
 
 
 #screen.blit(ground,(50,100))
@@ -34,10 +35,14 @@ while n < 6: # No. of blocks to be rendered atm
   i = 0
   w = random.randint(3,5) # w is width of platform
   y = random.randint(0,15) # y is height of platform
-  x = random.randint(0,15) 
+  x = random.randint(0,15)
+  c = random.randint(0,1) 
   while i < w:
     screen.blit(ground,(tw*x+tw*i,tw*y))
-    screen.blit(grass,(tw*x+tw*i,tw*y))
+    if c == 1:
+      screen.blit(grass,(tw*x+tw*i,tw*y))
+    else:
+      screen.blit(y_grass,(tw*x+tw*i,tw*y))  
     i = i+1
 pygame.display.flip()
 running = True
